baa1da49ff25e173d0282d3b095ee6ce80132f84
Michael Ida <31176432+drmikeida@users.noreply.github.com>
2017-10-16 06:16:46 -1000

    Add files via upload
    initial commit: 2017-10-16

---
+++ FactorialTester.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author drmikeida
+ * 
+ * Class to implement and test a method for
+ * calculating the factorial of a number.
+ *
+ */
+public class FactorialTester {
+	
+	public static int factorial(int n) {
+		/*
+		 * Write code to implement the factorial function.
+		 * 
+		 * The factorial of a number is the product of all of the
+		 * integers from 1 up to that number.  For example, the
+		 * factorial of 6 (written as 6! in mathematical notation)
+		 * is equal to 6 * 5 * 4 * 3 * 2 * 1.
+		 */
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("1! = " + factorial(1) + " (should be 1)");
+		System.out.println("2! = " + factorial(2) + " (should be 2)");
+		System.out.println("5! = " + factorial(5) + " (should be 120)");
+		System.out.println("10! = " + factorial(10) + " (should be equal 3628800");
+	}
+
+}
